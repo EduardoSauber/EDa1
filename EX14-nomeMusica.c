cd4ac69d626736f4ef88ed88c4726e80634864b9
Eduardo de Sousa Brito <edusoubrit09@gmail.com>
2026-01-16 13:14:01 -0300

    EX14

--- EX14-nomeMusica.c
+++ EX14-nomeMusica.c
@@ -18,7 +18,7 @@ typedef struct tipoBanda{
 
     char nome[80];
     int qtd;
-    tipoMusica musica[100];
+    tipoMusica musicas[100];
 }tipoBanda;
 
 void pesquisarNomeMusica(char pesquisa[80],tipoBanda bandas[50],int n){
@@ -26,11 +26,48 @@ void pesquisarNomeMusica(char pesquisa[80],tipoBanda bandas[50],int n){
     // bandas é a lista de bandas no sistema, onde cada uma tem uma lista de musicas
     // n a quantidade de bandas cadastradas
 
-    for(int index_banda = 0; index_banda <= n; index_banda++){
-        for(int index_musica = 0; index_musica <= sizeof(bandas[index_banda].musica); index_musica++){
+    // verificar se a musica foi encontrada no sistema
+    int res_pesquisa = 0;
 
+    // loop entre as bandas cadastradas
+    for(int index_bandas = 0; index_bandas < n; index_bandas++){
+
+        // loop entre as musicas cadastradas na banda
+        for(int index_musicas = 0; index_musicas < bandas[index_bandas].qtd; index_musicas++){
+
+            // comparar se o nome pesquisado existe na banda
+            int resultado = strcmp(bandas[index_bandas].musicas[index_musicas].nome,pesquisa);
+            if(resultado == 0){
+                // se o strcmp retorna 0 (as strings são iguais), imprimir o nome da banda e o ano
+                printf("%s : ano %d\n", bandas[index_bandas].nome,bandas[index_bandas].musicas[index_musicas].ano);
+                res_pesquisa = 1;   // avisar à função que foi encontrado um resultado
+            }
         }
     }
 
-    printf("%s : ano %d");
+    // verificar se a pesquisa teve algum resultado
+    if(res_pesquisa == 0){
+        // se não for encontrado nenhuma musica
+        printf("Musica nao encontrada\n");
+    }
 }
+
+/*
+int  main(){
+    tipoBanda debugBandas[50];
+    strcpy(debugBandas[0].nome,"DECO*27");
+    debugBandas[0].qtd = 2;
+    strcpy(debugBandas[0].musicas[0].nome, "TELEPATHY");
+    debugBandas[0].musicas[0].ano = 2025;
+    strcpy(debugBandas[0].musicas[1].nome, "RABBIT HOLE");
+    debugBandas[0].musicas[1].ano = 2025;
+
+    char input[80];
+    while (scanf("%[^\n]",input) != EOF){
+        pesquisarNomeMusica(input,debugBandas,1);
+        getchar();
+    }
+    
+    return 0;
+}
+*/
