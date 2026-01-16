803b7be0620260e98ae03c5784e933aa409c9ba4
Eduardo de Sousa Brito <edusoubrit09@gmail.com>
2026-01-15 21:48:03 -0300

    Finalização EX4

--- EX4-taxaDeReprovacao.c
+++ EX4-taxaDeReprovacao.c
@@ -2,22 +2,56 @@
 
 int main(){
     int qtd_disciplinas;
-    int chk = printf("d", &qtd_disciplinas);
 
-    if (chk != EOF){
-        int disciplinas[qtd_disciplinas];
+    // só pra ter certeza que enviaram algum dado pro programa
+    if (scanf("%d", &qtd_disciplinas) != EOF){
 
-        // escaneia o ano e o semestre atuais e a quantidade de turmas ofertadas 
+        // valores do ano, semestre e quantidade de materias ofertadas atualmente
         int ano,semestre,m_quantidade;
-        scanf("%d %d %d", ano, semestre, m_quantidade);
+        
+        // loop entre os anos e os semestres ofertados 
+        while(scanf("%d %d %d", &ano, &semestre, &m_quantidade) != EOF){
 
-        // 
-        for(int i = 1;i < m_quantidade;i++){
-            int materia[3];
-            // escaneia o codigo da materia, qtd de alunos inscritos, qtd de alunos reprovados
-            scanf("%d %d %d",&materia[0],&materia[1],&materia[2]);
+            // um array de mapeamento de todas as materias ofertadas
+            int mapa_materias[qtd_disciplinas];  // [codigo da materia]
 
+            // como o array tem todas as materias, limpamos o lixo para que todas tenham -1 reprovações
+            for(int i = 0; i <= qtd_disciplinas; i++){
+                mapa_materias[i] = -1;
+            }
+            int maior_reprovacao = -1; // maior reprovação entre as materias ofertadas
+
+            // loop para analisar a situação das materias ofertadas
+            for(int index = 0; index < m_quantidade; index++){
+                // o programa recebe o codigo, a qtd de matriculados, e a qtd de aprovados
+                int codigo,matriculados,aprovados;
+                scanf("%d %d %d",&codigo,&matriculados,&aprovados);
+
+                int reprovados = matriculados - aprovados;  // calculo para a qtd de alunos reprovados
+                // se a materia tiver uma qtd de reprovação maior, o valor de maior reprovação é alterado
+                if(reprovados > maior_reprovacao){
+                    maior_reprovacao = reprovados;
+                }
+
+                // envia o numero de reprovados no indice=codigo
+                mapa_materias[codigo] = reprovados;
+            }
+
+            // saida
+            printf("%d/%d\n",ano,semestre);
+            for(int k = 0; k < qtd_disciplinas; k++){
+                // verifica se a turma foi ofertada
+                if(mapa_materias[k] != -1){
+                    // se a materia tiver o numero de reprovaçÕes igual ao maior valor, imprime o codigo
+                    if(mapa_materias[k] == maior_reprovacao){
+                        printf("%d ",k);
+                    }
+                }
+            }
+            printf("\n\n");
+            
         }
+        
     }
     return 0;
 }
