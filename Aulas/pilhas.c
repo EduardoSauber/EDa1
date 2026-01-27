20a90ebbdb846313813f513cc003524d08a6b4b7
Eduardo de Sousa Brito <edusoubrit09@gmail.com>
2026-01-27 16:35:31 -0300

    Added filas.c & pilhas.c To Aulas

---
+++ Aulas/pilhas.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+/*
+
+LIFO - LAST IN, FIRST OUT
+
+--
+
+Inserção - push
+Remoção - pop
+
+--
+
+onde usar:
+    call stack
+    recursão
+    undo/redo
+
+--
+
+Cons:
+    Tamanho fixo
+    posições não reutilizáveis
+    overflow
+
+*/
+
+void criaPilha(int *t){
+
+}
+
+void pilhaVazia(int *t){
+    
+}
+
+void pilhaCheia(int *t, int N){
+
+}
+
+int empilha(int pilha[],int N,int *t,int y){
+    if (pilhaCheia(*t,N)) return 0;
+}
+
+int desempilha(int pilha[], int *t, int *x){
+    if (pilhaVazia(*t)) return 0;
+}
