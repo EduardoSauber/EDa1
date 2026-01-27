1050f437dcda4778ad5af081d7f920a97ecf6d25
Eduardo de Sousa Brito <edusoubrit09@gmail.com>
2026-01-27 16:18:28 -0300

    Added filas.c

---
+++ filas.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+int TAM = 5;
+
+void criarFila(int fila[], int N, int *p, int *u){
+    *u = 0;
+    *p = 0;
+}
+
+int filaVazia(int p, int u){
+    return (p == u);
+}
+
+int filaCheia(int u, int N){
+    return (u == N);
+}
+
+int colocaNaFila(int fila[], int N, int *u, int y){
+    if (filaCheia(*u,N)) return 0;
+
+    fila[*u] = y;
+    (*u)++;
+    return 1;
+}
+
+int tiraDaFila(int fila[], int *p, int u, int *x){
+    if (filaVazia(*p,u)) return 0;
+
+    *x = fila[*p];
+    (*p)++;
+    return 1;
+}
+
+int main(void){
+    int fila[TAM];
+}
